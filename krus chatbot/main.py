7f65867fdd31006c2bb5b273c18fd505e53538f2
Barbara Wozniak <barbara.wozniak@symmetry.pl>
2025-10-23 15:17:20 +0200

    chroma

--- krus chatbot/main.py
+++ krus chatbot/main.py
@@ -3,17 +3,26 @@ from tabular_data import answer
 
 print("AgroBot – wpisz pytanie (lub 'exit' aby wyjść)\n")
 
+key_words = ["dane"]
+used_module = None 
+
 while True:
     try:
         user_msg = input("Ty: ").strip()
     except EOFError:
         break
+
     if not user_msg:
+        print("(pusta wiadomość)\n")
         continue
-    if user_msg.lower() in {"exit","quit","q"}:
+
+    if user_msg.lower() in {"exit", "quit", "q"}:
         print("koniec")
         break
 
-    ust = ask(user_msg)  
-    data = answer(user_msg)
-    print("KRUS:", "\n", "USTAWA", "\n", ust, "\n", "DANE TABELARYCZNE", "\n", data, "\n")
+    if any(kw in user_msg.lower() for kw in key_words):
+        used_module = answer(user_msg)
+    else:
+        used_module = ask(user_msg)
+
+    print(used_module, "\n")
